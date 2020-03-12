328c3442c22655a4f07827595e2e5503ae11a263
Fay Wang <v-faywan@microsoft.com>
2020-03-12 15:41:54 +0800

    Add project files.

---
+++ GetKeyStateApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GetKeyStateApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Window.Current.Activated += Current_Activated;
+        }
+
+        private void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            bool state = IsCapsLockOn();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool state = IsCapsLockOn();
+        }
+
+        private bool IsCapsLockOn()
+        {
+            // check caps lock key state
+            var keystate = Window.Current.CoreWindow.GetKeyState(VirtualKey.CapitalLock);
+            return (keystate & CoreVirtualKeyStates.Locked) != 0;
+        }
+    }
+}
